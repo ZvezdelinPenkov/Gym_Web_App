391e13b076a3c52bbc6466f3de895084bcac1441
Zvezdelin Penkov <zvezdi2003@gmail.com>
2024-10-18 01:09:07 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Gym_Web_App
+ This repository contains our project for the course "Applied programming with Java", 2024/2025, winter semester
---
+++ src/main/java/org/example/gym_web_app/GymWebAppApplication.java
@@ -0,0 +1,13 @@
+package org.example.gym_web_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GymWebAppApplication.class, args);
+	}
+
+}
