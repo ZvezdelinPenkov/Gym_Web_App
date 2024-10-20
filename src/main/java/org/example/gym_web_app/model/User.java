bc0426ec27a23a90d472d8cf91dbd503a57d6697
Zvezdelin Penkov <zvezdi2003@gmail.com>
2024-10-20 22:36:08 +0300

    Added User class

--- src/main/java/org/example/gym_web_app/model/User.java
+++ src/main/java/org/example/gym_web_app/model/User.java
@@ -1,9 +1,14 @@
 package org.example.gym_web_app.model;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.HashSet;
 import java.util.Set;
 
+@Setter
+@Getter
 @Entity
 public class User {
 
@@ -23,44 +28,6 @@ public class User {
             inverseJoinColumns = @JoinColumn(name = "role_id"))
     private Set<Role> roles = new HashSet<>();
 
-    // Getters and Setters...
-
     public User() {}
 
-    public User(String username, String password) {
-        this.username = username;
-        this.password = password;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public Set<Role> getRoles() {
-        return roles;
-    }
-
-    public void setRoles(Set<Role> roles) {
-        this.roles = roles;
-    }
 }
