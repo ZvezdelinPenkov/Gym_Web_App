aa26395e544ef94b5a12fd0be312b9d025acea4e
Zvezdelin Penkov <zvezdi2003@gmail.com>
2024-10-28 19:08:06 +0200

    Updated User

--- src/main/java/org/example/gym_web_app/model/User.java
+++ src/main/java/org/example/gym_web_app/model/User.java
@@ -1,17 +1,12 @@
 package org.example.gym_web_app.model;
 
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
 
 import java.util.HashSet;
 import java.util.Set;
 
-@Setter
-@Getter
 @Entity
 public class User {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -33,6 +28,37 @@ public class User {
     public User(String username, String password) {
         this.username = username;
         this.password = password;
+    }
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<Role> roles) {
+        this.roles = roles;
     }
 
 }
