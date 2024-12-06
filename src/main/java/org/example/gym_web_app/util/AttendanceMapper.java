3aff6e46367a87f61dd5a7cb3245b86bf0400288
Zvezdelin Penkov <zvezdi2003@gmail.com>
2024-12-07 00:51:23 +0200

    Updated AttendanceMapper

--- src/main/java/org/example/gym_web_app/util/AttendanceMapper.java
+++ src/main/java/org/example/gym_web_app/util/AttendanceMapper.java
@@ -1,4 +1,29 @@
 package org.example.gym_web_app.util;
 
+import org.example.gym_web_app.dto.AttendanceDTO;
+import org.example.gym_web_app.model.Attendance;
+
 public class AttendanceMapper {
+
+    private AttendanceMapper() {
+
+    }
+
+    public static AttendanceDTO toDTO(Attendance attendance) {
+        AttendanceDTO dto = new AttendanceDTO();
+        dto.setId(attendance.getId());
+        dto.setMemberId(attendance.getMember() != null ? attendance.getMember().getId() : null);
+        dto.setClassScheduleId(attendance.getClassSchedule() != null ? attendance.getClassSchedule().getId() : null);
+        dto.setAttendanceTime(attendance.getAttendanceTime());
+        dto.setAttended(attendance.isAttended());
+        return dto;
+    }
+
+    public static Attendance toEntity(AttendanceDTO dto) {
+        Attendance attendance = new Attendance();
+        attendance.setId(dto.getId());
+        attendance.setAttendanceTime(dto.getAttendanceTime());
+        attendance.setAttended(dto.isAttended());
+        return attendance;
+    }
 }
