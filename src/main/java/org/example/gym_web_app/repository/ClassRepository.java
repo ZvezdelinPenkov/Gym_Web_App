4361ea7b47f8d820c1fd554f1c4bd2a70b9cf203
Zvezdelin Penkov <zvezdi2003@gmail.com>
2024-12-06 23:02:24 +0200

    Updated ClassRepository

--- src/main/java/org/example/gym_web_app/repository/ClassRepository.java
+++ src/main/java/org/example/gym_web_app/repository/ClassRepository.java
@@ -1,4 +1,9 @@
 package org.example.gym_web_app.repository;
 
-public interface ClassRepository {
+import org.example.gym_web_app.model.Class;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ClassRepository extends JpaRepository<Class, Long> {
 }
