e818d010d3a25595ed9c89b23bf16b9f815b534f
Zvezdelin Penkov <zvezdi2003@gmail.com>
2024-12-07 00:47:24 +0200

    Updated ClassScheduleDTO

--- src/main/java/org/example/gym_web_app/dto/ClassScheduleDTO.java
+++ src/main/java/org/example/gym_web_app/dto/ClassScheduleDTO.java
@@ -1,4 +1,54 @@
 package org.example.gym_web_app.dto;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 public class ClassScheduleDTO {
+
+    private Long id;
+    private Long classId;
+    private LocalDate date;
+    private LocalTime startTime;
+    private LocalTime endTime;
+
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getClassId() {
+        return classId;
+    }
+
+    public void setClassId(Long classId) {
+        this.classId = classId;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public LocalTime getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(LocalTime startTime) {
+        this.startTime = startTime;
+    }
+
+    public LocalTime getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(LocalTime endTime) {
+        this.endTime = endTime;
+    }
 }
