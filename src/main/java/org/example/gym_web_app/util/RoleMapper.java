d2343775d7167ea8c1b663ae912a11f37699d360
Zvezdelin Penkov <zvezdi2003@gmail.com>
2024-12-07 00:22:42 +0200

    Updated RoleMapper

--- src/main/java/org/example/gym_web_app/util/RoleMapper.java
+++ src/main/java/org/example/gym_web_app/util/RoleMapper.java
@@ -1,4 +1,24 @@
 package org.example.gym_web_app.util;
 
+import org.example.gym_web_app.dto.RoleDTO;
+import org.example.gym_web_app.model.Role;
+
 public class RoleMapper {
+
+    private RoleMapper() {
+    }
+
+    public static RoleDTO toDTO(Role role) {
+        RoleDTO dto = new RoleDTO();
+        dto.setId(role.getId());
+        dto.setName(role.getName());
+        return dto;
+    }
+
+    public static Role toEntity(RoleDTO dto) {
+        Role role = new Role();
+        role.setId(dto.getId());
+        role.setName(dto.getName());
+        return role;
+    }
 }
