27cb619b93eed265d768ca0a75b4027ccffeb9e7
Zvezdelin Penkov <zvezdi2003@gmail.com>
2024-12-06 23:14:54 +0200

    Updated ClassScheduleRepository

--- src/main/java/org/example/gym_web_app/repository/ClassScheduleRepository.java
+++ src/main/java/org/example/gym_web_app/repository/ClassScheduleRepository.java
@@ -2,6 +2,8 @@ package org.example.gym_web_app.repository;
 
 import org.example.gym_web_app.model.ClassSchedule;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ClassScheduleRepository extends JpaRepository<ClassSchedule, Long> {
 }
