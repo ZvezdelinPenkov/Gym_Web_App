4f37a2057b26f4c0d693813d3abc780aba4f413e
GerganaRoeva <gergana.roeva@gmail.com>
2024-12-16 17:20:56 +0200

    Swagger config

--- src/main/java/org/example/gym_web_app/config/OpenApiConfig.java
+++ src/main/java/org/example/gym_web_app/config/OpenApiConfig.java
@@ -1,12 +1,12 @@
 package org.example.gym_web_app.config;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.OpenAPI;
-import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class OpenApiConfig {
-    @Bean
+//    @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
                 .info(new Info()
