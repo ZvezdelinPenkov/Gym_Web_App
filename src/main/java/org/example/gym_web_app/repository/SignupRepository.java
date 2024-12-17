7ae22649a76c2a15f516b3fbb71a330cae8543cd
GerganaRoeva <gergana.roeva@gmail.com>
2024-12-18 00:22:02 +0200

    fixed query in signup repo

--- src/main/java/org/example/gym_web_app/repository/SignupRepository.java
+++ src/main/java/org/example/gym_web_app/repository/SignupRepository.java
@@ -10,6 +10,8 @@ public interface SignupRepository extends JpaRepository<Signup, Long> {
 
     long countByClassScheduleId(Long classScheduleId);
 
-    @Query("SELECT c.maxParticipants FROM Class c JOIN ClassSchedule cs ON c.id = cs.classId WHERE cs.id = :classScheduleId")
+    @Query("SELECT c.maxParticipants FROM Class c JOIN ClassSchedule cs ON c = cs.classEntity WHERE cs.id = :classScheduleId")
+
+
     long getMaxParticipantsByClassId(Long classScheduleId);
 }
