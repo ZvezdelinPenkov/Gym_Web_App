509b71b6742ae4eb4c2475748df9847ac0d85404
Zvezdelin Penkov <zvezdi2003@gmail.com>
2024-12-19 15:12:34 +0200

    Updated MemberRepository

--- src/main/java/org/example/gym_web_app/repository/MemberRepository.java
+++ src/main/java/org/example/gym_web_app/repository/MemberRepository.java
@@ -4,7 +4,9 @@ import org.example.gym_web_app.model.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
-
+    Optional<Object> findByEmail(String email);
 }
