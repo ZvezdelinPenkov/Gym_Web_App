22247ee2f697e1ade902bd5514c0d788780eaeda
Zvezdelin Penkov <zvezdi2003@gmail.com>
2024-12-23 01:44:46 +0200

    Added TestConfig

---
+++ src/test/java/org/example/gym_web_app/config/TestConfig.java
@@ -0,0 +1,22 @@
+package org.example.gym_web_app.config;
+
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+@TestConfiguration
+public class TestConfig {
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+    }
+}
