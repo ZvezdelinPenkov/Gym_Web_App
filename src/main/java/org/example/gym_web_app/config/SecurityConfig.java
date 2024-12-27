7453fe3ee816421c2c0d14982beb5dc87832cd28
Zvezdelin Penkov <zvezdi2003@gmail.com>
2024-12-28 00:40:03 +0200

    Updated SecurityConfig

--- src/main/java/org/example/gym_web_app/config/SecurityConfig.java
+++ src/main/java/org/example/gym_web_app/config/SecurityConfig.java
@@ -34,6 +34,7 @@ public class SecurityConfig {
                 .userDetailsService(userDetailsService)
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/auth/**",
+                                "/api/members/**",
                                 "/v3/api-docs/**",
                                 "/swagger-ui/**",
                                 "/swagger-ui.html").permitAll()
