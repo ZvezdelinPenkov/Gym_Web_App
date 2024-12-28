5e2b586e72bdbe230878d51a9734a49c83f6eb67
GerganaRoeva <gergana.roeva@gmail.com>
2024-12-28 22:30:24 +0200

    change id handling

--- src/main/java/org/example/gym_web_app/util/ClassMapper.java
+++ src/main/java/org/example/gym_web_app/util/ClassMapper.java
@@ -1,10 +1,19 @@
 package org.example.gym_web_app.util;
 
 import org.example.gym_web_app.dto.ClassDTO;
+import org.example.gym_web_app.exception.ResourceNotFoundException;
 import org.example.gym_web_app.model.Class;
+import org.example.gym_web_app.model.Users;
+import org.example.gym_web_app.repository.ClassRepository;
+import org.example.gym_web_app.repository.UsersRepository;
+
+import org.example.gym_web_app.service.UsersService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 import java.util.stream.Collectors;
 
+@Component
 public class ClassMapper {
 
     private ClassMapper() {
@@ -17,7 +26,7 @@ public class ClassMapper {
         dto.setTitle(classEntity.getTitle());
         dto.setDuration(classEntity.getDuration());
         dto.setMaxParticipants(classEntity.getMaxParticipants());
-        dto.setInstructorId(classEntity.getInstructor() != null ? classEntity.getInstructor().getId() : null);
+        dto.setInstructorId(classEntity.getInstructor().getId());
         dto.setScheduleIds(
                 classEntity.getSchedules().stream()
                         .map(schedule -> schedule.getId())
@@ -26,12 +35,18 @@ public class ClassMapper {
         return dto;
     }
 
-    public static Class toEntity(ClassDTO dto) {
+    public static Class toEntity(ClassDTO dto, UsersRepository usersRepository) {
+
+        Users user = usersRepository.findById(dto.getInstructorId())
+                .orElseThrow(() -> new ResourceNotFoundException("User not found with id"));
+
         Class classEntity = new Class();
         classEntity.setId(dto.getId());
         classEntity.setTitle(dto.getTitle());
         classEntity.setDuration(dto.getDuration());
         classEntity.setMaxParticipants(dto.getMaxParticipants());
+//        classEntity.setInstructor(dto.getInstructorId());
+        classEntity.setInstructor(user);
         return classEntity;
     }
 }
